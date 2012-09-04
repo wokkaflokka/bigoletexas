dc2893dcf4df6b47a6a222276d88473cc7299269
wokkaflokka <emery.coxe@gmail.com>
2012-09-04 00:21:30 -0600

    draw the outline of texas

---
+++ ManglePS.rb
@@ -0,0 +1,90 @@
+#!/usr/bin/env ruby
+
+################################################
+#
+# A very crude script that parses a very specific
+# PostScript file in order to draw the shape of Texas in 
+# Java. Outputs all necessary coordinates, and the 
+# appropriate command sequence to recreate the drawing
+#
+################################################
+
+file = 'Texas.eps'
+
+commands = []
+xs = []
+ys = []
+mx, my = nil
+lx, ly = nil
+
+cx1,cx2,cx3 = nil
+cy1,cy2,cy3 = nil
+
+File.open(file,'r').each_line do |line|
+  t = line.chomp("\n")
+  if t.include?('move')
+    temp = t.split(' ')
+    mx   = temp[0]
+    my   = temp[1]
+    commands << 'moveTo'
+    xs << mx
+    ys << my
+  elsif t.include?('line')
+    temp = t.split(' ')
+    lx   = temp[0]
+    ly   = temp[1]
+    commands << 'lineTo'
+    xs << lx
+    ys << ly
+  elsif t.include?('curve')
+    temp = t.split(' ')
+    cx1  = temp[0]
+    cy1  = temp[1]
+    cx2  = temp[2]
+    cy2  = temp[3]
+    cx3  = temp[4]
+    cy3  = temp[5]
+    commands << 'curveTo'
+    xs << cx1
+    xs << cx2
+    xs << cx3
+    ys << cy1
+    ys << cy2
+    ys << cy3
+  end
+end
+
+xstring = 'int xPoints[] = {'
+ystring = 'int yPoints[] = {'
+max = xs.size
+xs.each_with_index do |x,i|
+  xstring << "#{x}, " unless i == (max -1)
+  xstring << "#{x}" if i == (max -1)
+end
+xstring << '};'
+ystring = 'int yPoints[] = {'
+max = ys.size
+ys.each_with_index do |y,i|
+  ystring << "#{y}, " unless i == (max -1)
+  ystring << "#{y}" if i == (max -1)
+end
+ystring << '};'
+
+i = 0
+File.open('java-strings.tmp','w') do |file|
+  file.write(xstring + "\n\n")
+  file.write(ystring + "\n\n")
+  commands.each do |command|
+    if command.include?('curve')
+      file.write("texas.curveTo(xPoints[#{i}],yPoints[#{i}],xPoints[#{i+1}],yPoints[#{i+1}],xPoints[#{i+2}],yPoints[#{i+2}]);\n")
+      i += 2
+    elsif command.include?('line')
+      file.write("texas.lineTo(xPoints[#{i}],yPoints[#{i}]);\n")
+    else
+      file.write("texas.moveTo(xPoints[#{i}],yPoints[#{i}]);\n")
+    end
+    i += 1
+  end
+end
+
+exit(0)
